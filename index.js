512c1b5fd68c37f62d7fb85d6e6c8fec829350ac
feildmaster <feildmaster@users.noreply.github.com>
2018-01-02 13:49:51 -0600

    feat: Log even when we don't have all "required" information

--- index.js
+++ index.js
@@ -14,23 +14,20 @@ const rootRef = admin.database().ref();
 rootRef.child('messages').orderByChild('timeReceived').startAt(Date.now()).on('child_added', (e) => {
   let msg = e.val();
 
-  // we can only log for messages that have the info we need.
-  if (msg.extra_client_info) {
-    let extra = msg.extra_client_info;
-    let direction = msg.direction === 'in' ? 'IN :' : 'OUT:';
-    let connector = `${(extra.connectorType || 'Unknown')}/${(extra.connectorName || msg.cid || 'Unknown')}`;
-    let server = extra.server || 'Unknown';
-    let channel = extra.channel || 'Unknown';
-    let source = extra.source || 'Unknown';
-    let plugin = '';
+  let extra = msg.extra_client_info || {};
+  let direction = msg.direction === 'in' ? 'IN :' : 'OUT:';
+  let connector = `${(extra.connectorType || 'Unknown')}/${(extra.connectorName || msg.cid || 'Unknown')}`;
+  let server = extra.server || 'Unknown';
+  let channel = extra.channel || 'Unknown';
+  let source = extra.source || 'Unknown';
+  let plugin = '';
 
-    if (msg.direction === 'out') {
-      plugin = `(${extra.pluginName}/${extra.pluginInstance})`;
-      source = extra.connectorBotName || 'Unknown';
-    }
-
-    let logMsg = `${direction} [${e.key}] {${connector}} ${server}/${channel}: ${source} ${plugin}`;
-
-    logger.info(logMsg);
+  if (msg.direction === 'out') {
+    plugin = `(${extra.pluginName}/${extra.pluginInstance})`;
+    source = extra.connectorBotName || 'Unknown';
   }
+
+  let logMsg = `${direction} [${e.key}] {${connector}} ${server}/${channel}: ${source} ${plugin}`;
+
+  logger.info(logMsg);
 });
